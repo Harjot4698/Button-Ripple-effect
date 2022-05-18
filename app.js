a44b84449fed75837785eb37bab2d2f1d8a4be32
harjot <harjot23062000@gmail.com>
2022-05-17 17:09:52 -0700

    done project

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const buttons=document.querySelectorAll('.ripple')
 
-buttons.forEach(button=>{
-    button.addEventListener('click',function (e){
-        const x=e.clientX
-        const y=e.clientY
-        const buttonTop=e.target.offsetTop
-        const buttonleft=e.target.offsetLeft
+   
+const buttons = document.querySelectorAll('.ripple')
 
-        const xInside=x-buttonleft
-        const yInside=y-buttonTop
-        console.log(xInside,yInside)
-        const circle=document.createElement('span')
-        circle.classList.add('.circle')
-        circle.style.top=yInside+'px'
-        circle.style.left=xInside+'px'
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
 
         this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
     })
 })
